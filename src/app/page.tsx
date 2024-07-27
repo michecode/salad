f9e76e5c13a2e50a5874230e055723670e0d1e89
michecode <mfrs@protonmail.com>
2024-07-26 23:43:29 -0700

    simple shop route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
 import { Button } from "./_components/ui/button";
-
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen">
       <div className="flex w-1/2 mx-auto flex-col items-center justify-center space-y-4">
         <h1 className="text-center text-6xl font-serif font-bold">Buy Archival Art from Museums Across the Country</h1>
-        <Button>Shop</Button>
+        <Link href="/shop">
+          <Button>Shop</Button>
+        </Link>
       </div>
     </main>
   );
---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default async function Shop() {
+  return (
+    <div>
+      hello
+    </div>
+  );
+};
