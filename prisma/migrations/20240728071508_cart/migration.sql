a4bbb18d49832701f859225a1db192e53133738c
michecode <mfrs@protonmail.com>
2024-07-28 00:15:41 -0700

    cart model

---
+++ prisma/migrations/20240728071508_cart/migration.sql
@@ -0,0 +1,55 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `totalAmount` on the `Order` table. All the data in the column will be lost.
+  - You are about to alter the column `price` on the `OrderItem` table. The data in that column could be lost. The data in that column will be cast from `Float` to `Int`.
+  - Added the required column `totalCost` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "CartItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "cartId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "orderDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "totalCost" INTEGER NOT NULL,
+    "recipient" TEXT NOT NULL,
+    "address" TEXT NOT NULL
+);
+INSERT INTO "new_Order" ("address", "id", "orderDate", "recipient") SELECT "address", "id", "orderDate", "recipient" FROM "Order";
+DROP TABLE "Order";
+ALTER TABLE "new_Order" RENAME TO "Order";
+CREATE TABLE "new_OrderItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "orderId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_OrderItem" ("id", "orderId", "price", "productId", "quantity") SELECT "id", "orderId", "price", "productId", "quantity" FROM "OrderItem";
+DROP TABLE "OrderItem";
+ALTER TABLE "new_OrderItem" RENAME TO "OrderItem";
+CREATE UNIQUE INDEX "OrderItem_orderId_key" ON "OrderItem"("orderId");
+CREATE UNIQUE INDEX "OrderItem_productId_key" ON "OrderItem"("productId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
