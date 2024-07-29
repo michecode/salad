ba83b337a76c473ba8e41a8cd4e2c35d6654c81b
michecode <mfrs@protonmail.com>
2024-07-29 02:23:03 -0700

    pagination

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,21 +1,71 @@
 import { ProductCard } from "../_components/product-card";
-import { getAllProducts } from "~/lib/product";
+import { getAllProducts, totalProductPages } from "~/lib/product";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "../_components/ui/pagination";
 
-export default async function Shop() {
-  const products = await getAllProducts();
+export default async function Shop({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const totalPages = await totalProductPages();
+  const page = Number(searchParams.page) || 1;
+  const products = await getAllProducts(page);
 
   return (
-    <main className="flex">
-      <div className="w-1/4">
-        future filters
-      </div>
-      <div className="w-3/4 grid grid-cols-3 gap-4">
-        {products.map((product) => {
-          return (
-            <ProductCard product={product} key={product.id}/>
-          );
-        })}
+    <main className="flex mb-10">
+      <div className="w-1/4">future filters</div>
+      <div className="w-3/4 space-y-4">
+        <div className="grid grid-cols-3 gap-4">
+          {products.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+        </div>
+          <div className="w-full">
+            <Pagination className="mx-auto">
+              <PaginationContent>
+                {page > 1 && <PaginationItem>
+                  <PaginationPrevious href={`/shop?page=${page-1}`} />
+                </PaginationItem>}
+                {page > 2 && <>
+                  <PaginationItem>
+                    <PaginationLink href="/shop?page=1">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                </>}
+                {page > 1 && <PaginationItem>
+                  <PaginationLink href={`/shop?page=${page-1}`}>{page-1}</PaginationLink>
+                </PaginationItem>}
+                <PaginationItem>
+                  <PaginationLink href="#" isActive>{page}</PaginationLink>
+                </PaginationItem>
+                {page + 1 <= totalPages && <PaginationItem>
+                  <PaginationLink href={`/shop?page=${page+1}`}>{page+1}</PaginationLink>
+                </PaginationItem>}
+                {page < (totalPages - 1) && <>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href={`/shop?page=${totalPages}`}>{totalPages}</PaginationLink>
+                  </PaginationItem>
+                </>}
+                {page < totalPages && <PaginationItem>
+                  <PaginationNext href={`/shop?page=${page+1}`} />
+                </PaginationItem>}
+              </PaginationContent>
+            </Pagination>
+          </div>
       </div>
     </main>
   );
-};
+}
--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,15 +1,33 @@
 import { db } from "~/server/db";
 
-export const getProduct = async ({ productId }: { productId: string | null }) => {
+const PAGE_SIZE = 51; // Divisible by 3 since theres 3 cols
+
+export const getProduct = async ({
+  productId,
+}: {
+  productId: string | null;
+}) => {
   const product = await db.product.findUnique({
     where: {
       id: productId ?? undefined,
-    }
+    },
   });
   return product;
-}
+};
 
-export const getAllProducts = async () => {
-  const products = await db.product.findMany();
+export const totalProductPages = async () => {
+  const totalProducts = await db.product.count();
+  return Math.ceil(totalProducts / PAGE_SIZE);
+};
+
+export const getAllProducts = async (page: number = 1) => {
+  const products = await db.product.findMany({
+    skip: (page - 1) * PAGE_SIZE,
+    take: PAGE_SIZE,
+    orderBy: {
+      id: "asc",
+    },
+  });
+
   return products ?? null;
-}
+};
