6b60d786960d361d12e42fdd460b2ee99d054a1d
michecode <mfrs@protonmail.com>
2024-07-29 01:40:18 -0700

    home page css tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import { Button } from "./_components/ui/button";
+import HeroImage from "./assets/museum-hero.jpg";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen">
-      <div className="flex w-1/2 mx-auto flex-col items-center justify-center space-y-4">
-        <h1 className="text-center text-6xl font-serif font-bold">Buy Archival Art from Museums Across the Country</h1>
+    <main className="flex max-h-[90vh] items-center">
+      <img src={HeroImage.src} className="w-1/4 rounded-xl ml-32 mt-20" />
+      <div className="mx-auto flex w-1/2 flex-col items-center justify-center space-y-4">
+        <h1 className="text-center font-serif text-6xl font-bold">
+          Buy Archival Art from Museums Across the Country
+        </h1>
         <Link href="/shop">
           <Button>Shop</Button>
         </Link>
       </div>
     </main>
   );
-};
+}
